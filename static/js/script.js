34eb92414ee816dfd6d7746cf533b273c900438b
Conal84 <conal.walsh1@gmail.com>
2020-05-29 13:19:32 +0000

    Changed emailjs script to remove contact number

--- static/js/script.js
+++ static/js/script.js
@@ -19,8 +19,6 @@ window.onload = function () {
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-      // generate the contact number value
-      this.contact_number.value = (Math.random() * 100000) | 0;
       emailjs.sendForm("contact_service", "contact_form", this);
     });
 };
