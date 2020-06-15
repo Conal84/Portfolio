92c667f77d07fd8653aa4f93d3c4831fa117f817
Conal84 <conal.walsh1@gmail.com>
2020-06-15 15:51:47 +0000

    Added Flask wtf, flash message, login form and validation

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, flash, redirect, request, url_for, session
 from flask_pymongo import PyMongo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 from bson.objectid import ObjectId
 from markupsafe import escape
 from os import path
@@ -14,6 +17,12 @@ app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.secret_key = os.environ.get('SECRET_KEY')
 
 mongo = PyMongo(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
 
 
 @app.route('/')
@@ -33,10 +42,12 @@ def project(project_id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'Log in here'
-    return render_template("pages/login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(
+            form.username.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
