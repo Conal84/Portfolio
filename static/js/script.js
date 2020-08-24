509297ba9d7d585a5e5d51b7c570e8570aff1743
Conal84 <conal.walsh1@gmail.com>
2020-08-24 20:18:39 +0000

    Added canvas polygons and changed colors

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // Function to animate skills progress circle
   function Circle(el) {
     $(el)
-      .circleProgress({ fill: { color: "#ffcd24" } })
+      .circleProgress({ fill: { color: "#ffd447" } })
       .on("circle-animation-progress", function (event, progress, stepValue) {
         $(this)
           .find("strong")
@@ -19,6 +19,7 @@ $(document).ready(function () {
   canv.height = window.innerHeight - $(".navbar").height();
   let c = canv.getContext("2d");
 
+  // Class to hold line information and calculate points on a line
   class DrawLine {
     constructor(startX, startY, endX, endY) {
       this.startX = startX;
@@ -44,11 +45,14 @@ $(document).ready(function () {
 
   let maxHeight = window.innerHeight;
   let maxWidth = window.innerWidth;
+
+  // Create 2 lines and call their calcpoints method
   let line1 = new DrawLine(0, maxHeight * 0.7, maxWidth, maxHeight * 0.4);
   let line2 = new DrawLine(maxWidth * 0.3, 0, maxWidth * 0.8, maxWidth);
   line1.calcpoints();
   line2.calcpoints();
 
+  // A function to draw a line
   function draw(coords, color, width, speed, segments) {
     let num = 1;
     inter = setInterval(change, speed);
@@ -68,14 +72,20 @@ $(document).ready(function () {
     }
   }
 
+  // Draw lines
+  draw(line1.points, "#ffcd24", 8, 20, 20);
+  draw(line2.points, "#ffcd24", 10, 20, 20);
+
+  // Function to find the x, y intercept of 2 lines
   function findIntercept(slope1, slope2, c1, c2) {
     let point = [];
     let x = (c2 - c1) / (slope1 - slope2);
     let y = slope1 * x + c1;
-    point.push({x: x, y: y});
+    point.push({ x: x, y: y });
     return point;
   }
 
+  // Function to draw colored polygons on the canvas
   function drawPoly(point1, point2, point3, point4, color) {
     c.fillStyle = color;
     c.beginPath();
@@ -87,21 +97,56 @@ $(document).ready(function () {
     c.fill();
   }
 
-  let interPoint = findIntercept(line1.slope, line2.slope, line1.intercept, line2.intercept);
-  console.log(interPoint);
+  // Call findIntercept with slopes and y intercepts of line1 and line2 to find x, y intercept
+  let interPoint = findIntercept(
+    line1.slope,
+    line2.slope,
+    line1.intercept,
+    line2.intercept
+  );
 
-  let canvCorners = [{x: 0, y: maxHeight}, {x: maxWidth, y: maxHeight}, {x: maxWidth, y: 0}];
+  // An array to hold the corners of the canvas
+  let canvCorners = [
+    { x: 0, y: 0 },
+    { x: maxWidth, y: 0 },
+    { x: maxWidth, y: maxHeight },
+    { x: 0, y: maxHeight },
+  ];
 
-  drawPoly(line1.points[0], canvCorners[0], line2.points[line2.points.length - 1], interPoint[0], "rgba(255, 205, 36, 0.7)");
+  drawPoly(
+    canvCorners[0],
+    line2.points[0],
+    interPoint[0],
+    line1.points[0],
+    "rgba(11, 10, 7, 1)"
+  ); // Top left Poly Rich Black
+  drawPoly(
+    line1.points[0],
+    canvCorners[3],
+    line2.points[line2.points.length - 1],
+    interPoint[0],
+    "rgba(234, 236, 236, 1)"
+  ); // Bottom Left Poly Platinum Grey
+  drawPoly(
+    line2.points[0],
+    canvCorners[1],
+    line1.points[line1.points.length - 1],
+    interPoint[0],
+    "rgba(255, 217, 92, 1)"
+  ); // Top Right Poly Mustard
+  drawPoly(
+    line1.points[line1.points.length - 1],
+    canvCorners[2],
+    line2.points[line2.points.length - 1],
+    interPoint[0],
+    "rgba(255, 205, 36, 1)"
+  ); // Bottom Right Poly Sunglow
 
   let typed = new Typed("#typed", {
     stringsElement: "#typed-strings",
     typeSpeed: 60,
     showCursor: false,
   });
-
-  draw(line1.points, "#FF0000", 8, 20, 20);
-  draw(line2.points, "#FFFFFF", 5, 20, 20);
 
   // Initialise emailjs
   (function () {
