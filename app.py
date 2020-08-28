6673f63a39036aadc438034dd4b52d06f50312b5
Conal84 <conal.walsh1@gmail.com>
2020-08-28 20:41:19 +0000

    Added keyframe animation to canvas divs

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def not_found_error(error):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', "0.0.0.0"),
             port=int(os.environ.get('PORT', "5000")),
-            debug=False)
+            debug=True)
